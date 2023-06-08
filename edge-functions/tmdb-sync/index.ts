fd1db6855e739150f0f4f6fc69e0406a7000b325
Marvin <marvin@poopjournal.rocks>
2023-06-08 23:44:18 +0200

    Deploy Edge Function

--- edge-functions/tmdb-sync/index.ts
+++
@@ -1,9 +0,0 @@
-// Follow this setup guide to integrate the Deno language server with your editor:
-// https://deno.land/manual/getting_started/setup_your_environment
-// This enables autocomplete, go to definition, etc.
-
-import { serve } from 'std/server'
-import { createClient } from '@supabase/supabase-js'
-import { corsHeaders } from '../_shared/cors.ts'
-
-console.log(`Function "tmdb-sync" up and running!`)
